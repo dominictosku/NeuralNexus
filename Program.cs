0c0311a4f13bee99a008363ba74f4664aea6c57a
dominictosku <dominic.tosku@gmail.com>
2025-01-25 15:50:40 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using OpenAI;
+using OpenAI.Chat;
+using System.Net;
+
+var client = new OpenAIClient(new("ollama"), new() { Endpoint = new("http://localhost:11434/v1") })
+    .GetChatClient("deepseek-r1:7b");
+
+ChatCompletion completion = client.CompleteChat("How do i cook spaghetti?");
+
+Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
